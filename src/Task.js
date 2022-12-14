b0238a83f45388a10f6bf9587d0bf5c5683d26b9
Faizunnazif Al Barohin <faizoen75@gmail.com>
2022-12-15 05:15:46 +0700

    update file task.js

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const url = "https://fe2612933-photos-gallery.netlify.app/" // TODO: replace this
+const studentName = "Faizunnazif Al Barohin" // TODO: replace this
+const studentId = "FE2612933" // TODO: replace this
 
 export { url, studentName, studentId }
